4c7144d194187cc72576c939c4365d0c997278ba
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-21 19:50:06 +0500

    feat: update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths(), svgr()],
+  build: {
+    minify: true,
+  },
+  publicDir: "assets",
+  preview: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
