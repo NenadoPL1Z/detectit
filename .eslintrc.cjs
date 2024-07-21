90380cf6297ef6af03ca0afb20e42bc4d6fb50ea
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-21 20:07:21 +0500

    feat: linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,23 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    "prettier",
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    "plugin:react/recommended",
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+      'react-refresh',
+      "react",
+      "prettier",
+      "@typescript-eslint"
+  ],
   rules: {
+    "prettier/prettier": "error",
+    "no-console": "warn",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
