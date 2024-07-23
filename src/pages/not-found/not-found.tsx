a04487a50a3b242cd38dc63c845ebcadd1a906c7
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-24 03:04:48 +0500

    feat: update

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -6,17 +6,19 @@ import styles from "./not-found.module.css";
 export const NotFoundPage = () => {
   return (
     <Flex grow="1" align="center" justify="center">
-      <Typography variant="b700" fontSize={350} lineHeight={300}>
-        404
-      </Typography>
-      <Typography className={styles.wrapText} variant="m500" fontSize={48} centered>
-        В данный момент идет расследование этого дела,{"\n"} а пока вы можете вернуться на{" "}
-        <Link to={NavigationRoutes.ROOT}>
-          <Typography tag="span" variant="m700" fontSize={48} underline centered>
-            главную
-          </Typography>
-        </Link>
-      </Typography>
+      <Flex className={styles.wrapper} align="center">
+        <Typography variant="b700" fontSize={350} lineHeight={300}>
+          404
+        </Typography>
+        <Typography variant="m500" fontSize={48} centered>
+          В данный момент идет расследование этого дела, а пока вы можете вернуться на{" "}
+          <Link to={NavigationRoutes.ROOT}>
+            <Typography tag="span" variant="m700" fontSize={48} underline centered>
+              главную
+            </Typography>
+          </Link>
+        </Typography>
+      </Flex>
     </Flex>
   );
 };
