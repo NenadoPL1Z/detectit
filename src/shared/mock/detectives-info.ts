faed1f54c4e666f053427fa739e6450ff80c4706
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-26 01:50:21 +0500

    feat: add images

---
+++ src/shared/mock/detectives-info.ts
@@ -0,0 +1,10 @@
+const BASE_IMG_ULR = "/public/assets/images/detectives";
+export const detectivesInfo = [
+  {
+    rank: "Стажер",
+    points: 0,
+    publicImage: `${BASE_IMG_ULR}/trainee.png`,
+    description:
+      "Ваша команда только начала свой детективный путь. Мы вам завидуем, ведь впереди столько всего интересного!",
+  },
+];
