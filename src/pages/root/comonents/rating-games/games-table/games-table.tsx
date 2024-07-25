f2015b5a6df96e150be8064e3f7b364162f4197c
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-26 01:10:18 +0500

    feat: update

--- src/pages/root/comonents/rating-games/games-table/games-table.tsx
+++ src/pages/root/comonents/rating-games/games-table/games-table.tsx
@@ -1,9 +1,10 @@
 import { GamesTableProps } from "./types";
 import Spinner from "@shared/assets/icons/spinner.svg?react";
 import { Typography } from "@shared/ui";
+import styles from "./games-table.module.css";
 export const GamesTable = ({ games, loading }: GamesTableProps) => {
   return (
-    <section>
+    <section className={styles.container}>
       <Typography variant="m400" fontSize={18}>
         {JSON.stringify(games.map((item) => item.team))}
       </Typography>
