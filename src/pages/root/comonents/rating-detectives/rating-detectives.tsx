6936cfbb7130110d31644a06974ddf6d97a69b3b
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-29 01:05:32 +0500

    feat: adaptive

--- src/pages/root/comonents/rating-detectives/rating-detectives.tsx
+++ src/pages/root/comonents/rating-detectives/rating-detectives.tsx
@@ -5,7 +5,7 @@ import { DetectivesSwiper } from "./detectives-swiper";
 export const RatingDetectives = () => {
   return (
     <section className={styles.container}>
-      <Typography className={styles.title} variant="b700" fontSize={64} centered>
+      <Typography className={styles.title} variant="b700" centered>
         СИСТЕМА РЕЙТИНГА
       </Typography>
       <DetectivesSwiper />
