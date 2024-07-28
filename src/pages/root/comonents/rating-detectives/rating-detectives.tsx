948d7963da78ea9fdcf563c9e989e3b110c7d3dc
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-28 23:41:50 +0500

    feat: add swiper

--- src/pages/root/comonents/rating-detectives/rating-detectives.tsx
+++ src/pages/root/comonents/rating-detectives/rating-detectives.tsx
@@ -1,18 +1,25 @@
-import { Flex, Typography } from "@shared/ui";
+import { Typography } from "@shared/ui";
 import styles from "./rating-detectives.module.css";
 import { DetectiveCard } from "./detective-card";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
 import { detectivesInfo } from "@shared/mock/detectives-info";
+import { Navigation } from "swiper/modules";
+
 export const RatingDetectives = () => {
   return (
     <section className={styles.container}>
       <Typography className={styles.title} variant="b700" fontSize={64} centered>
         СИСТЕМА РЕЙТИНГА
       </Typography>
-      <Flex vertical={false} gap={12} className={styles.detectives}>
+      <Swiper modules={[Navigation]} spaceBetween={110} loop slidesPerView="auto" navigation>
         {detectivesInfo.map((detective, index) => (
-          <DetectiveCard key={detective.id} index={index + 1} {...detective} />
+          <SwiperSlide className={styles.slide} key={detective.id}>
+            <DetectiveCard index={index + 1} {...detective} />
+          </SwiperSlide>
         ))}
-      </Flex>
+      </Swiper>
     </section>
   );
 };
