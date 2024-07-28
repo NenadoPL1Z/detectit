0e09c63ef04967145a2e502fcb89937638aedd25
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-29 00:52:10 +0500

    feat: footer adaptive

--- src/app/layout/main-layout/components/footer/footer.tsx
+++ src/app/layout/main-layout/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 import { Flex, Typography } from "@shared/ui";
 import styles from "./footer.module.css";
-import Logo from "@shared/assets/icons/logo-footer.svg?react";
+import Logo from "@shared/assets/icons/logo.svg?react";
 import VK from "@shared/assets/icons/vk.svg?react";
 import INST from "@shared/assets/icons/inst.svg?react";
 import { staticInfo } from "@shared/mock/static-info";
@@ -10,22 +10,22 @@ export const Footer = () => {
     <footer className={styles.footer}>
       <Flex className={styles.wrapper} align="center">
         <Logo className={styles.logo} />
-        <Flex vertical={false} gap={36}>
+        <Flex className={styles.info} vertical={false}>
           <a href={staticInfo.vk} target="_blank" rel="noreferrer">
-            <VK />
+            <VK className={styles.icon} />
           </a>
           <Flex align="center">
-            <Typography className={styles.game} variant="m400" fontSize={16} lineHeight={18} color="gray">
+            <Typography className={styles.questions} variant="m400" color="gray">
               Любые вопросы по играм
             </Typography>
             <a href={`tel://${staticInfo.tel.call}`}>
-              <Typography tag="span" variant="m700" fontSize={20} lineHeight={29} centered>
+              <Typography className={styles.phone} tag="span" variant="m700" centered>
                 {staticInfo.tel.displayUser}
               </Typography>
             </a>
           </Flex>
           <a href={staticInfo.inst} target="_blank" rel="noreferrer">
-            <INST />
+            <INST className={styles.icon} />
           </a>
         </Flex>
       </Flex>
