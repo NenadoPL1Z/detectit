09b47ebf688e405caaba95e442334c554b4e065b
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-28 17:12:05 +0500

    feat: add images

--- src/pages/root/comonents/rating-detectives/detective-card/detective-card.tsx
+++ src/pages/root/comonents/rating-detectives/detective-card/detective-card.tsx
@@ -1,6 +1,18 @@
 import { DetectiveCardProps } from "./types";
 import styles from "./detective-card.module.css";
+import { Flex, Typography } from "@shared/ui";
 
-export const DetectiveCard = ({ rank }: DetectiveCardProps) => {
-  return <div className={styles.container}>{rank}</div>;
+export const DetectiveCard = ({ rank, index }: DetectiveCardProps) => {
+  return (
+    <div className={styles.container}>
+      <Flex className={styles.top} vertical={false} align="center" gap={8}>
+        <Typography variant="m500" fontSize={8} color="black">
+          Дело No.{index > 9 ? `000${index}` : `0000${index}`}
+        </Typography>
+        <Typography variant="m500" fontSize={24} color="black">
+          {rank}
+        </Typography>
+      </Flex>
+    </div>
+  );
 };
