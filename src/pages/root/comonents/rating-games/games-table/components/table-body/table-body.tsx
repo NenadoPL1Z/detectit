af0b1226fb436ed95007863a861e5ddb010aab7a
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-07-28 21:45:32 +0500

    feat: table

--- src/pages/root/comonents/rating-games/games-table/components/table-body/table-body.tsx
+++ src/pages/root/comonents/rating-games/games-table/components/table-body/table-body.tsx
@@ -15,17 +15,17 @@ export const TableBody = ({
 }: TableBodyProps) => {
   return (
     <tr>
-      <th className={cn(styles.body, { "align-left": true })}>
+      <th className={cn(styles.body, styles.team, { "align-left": true })}>
         {number + 1}.&nbsp;&nbsp;&nbsp;
         {team}
       </th>
-      <th className={cn(styles.body)}>{total_points}</th>
-      <th className={cn(styles.body)}>{rank}</th>
-      <th className={cn(styles.body)}>{total_games}</th>
-      <th className={cn(styles.body)}> </th>
-      <th className={cn(styles.body)}>{first_places}</th>
-      <th className={cn(styles.body)}>{second_places}</th>
-      <th className={cn(styles.body, { "border-off": true })}>{third_places}</th>
+      <th className={cn(styles.body, styles["total-points"])}>{total_points}</th>
+      <th className={cn(styles.body, styles["rank"])}>{rank}</th>
+      <th className={cn(styles.body, styles["total-games"])}>{total_games}</th>
+      <th className={cn(styles.body, styles["empty"])}> </th>
+      <th className={cn(styles.body, styles["first-places"])}>{first_places}</th>
+      <th className={cn(styles.body, styles["second-places"])}>{second_places}</th>
+      <th className={cn(styles.body, styles["third-places"], { "border-off": true })}>{third_places}</th>
     </tr>
   );
 };
