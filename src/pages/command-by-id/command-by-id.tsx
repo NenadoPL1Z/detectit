a4dc4b66e62df3e19e6e6f80ac27a72fcdf3438e
NenadoPL1Z <limon4ikpro1@gmail.com>
2024-08-06 00:45:59 +0500

    feat: add state

--- src/pages/command-by-id/command-by-id.tsx
+++ src/pages/command-by-id/command-by-id.tsx
@@ -7,13 +7,20 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { GameModel } from "@shared/types";
 import { NavigationRoutes } from "@shared/constants";
+import Spinner from "@shared/assets/icons/spinner.svg?react";
 
 export const CommandByIdPage = () => {
   const { state } = useLocation();
-  // const [isLoading, setIsLoading] = useState<boolean>(!state);
-  // const [error, setError] = useState("");
-  const [game] = useState<GameModel>(state);
   const navigate = useNavigate();
+
+  const [game] = useState<GameModel>(state);
+  const [isLoading] = useState<boolean>(!state);
+  const [error] = useState("");
+  const isState = isLoading || error;
+
+  const handleRetry = () => {
+    //
+  };
 
   useEffect(() => {
     window.scrollTo({ top: 0 });
@@ -25,17 +32,32 @@ export const CommandByIdPage = () => {
         статистика команды
       </Typography>
       <Typography variant="m500" className={styles.subtitle}>
-        {game?.team}
+        {game?.team ?? "Загрузка..."}
       </Typography>
       <Flex className={styles.content} vertical={false}>
         <div className={styles.left}>
           <img className={styles.case} src={CaseImg} alt="deal" />
         </div>
         <div className={styles.right}>
-          <>
-            <CommandPhoto total_points={game.total_points} />
-            <CommandContent {...game} />
-          </>
+          {isState && (
+            <Flex className={styles.state} align="center" justify="center">
+              {isLoading && <Spinner className={styles.spinner} />}
+              {error && (
+                <Flex align="center">
+                  <Typography variant="m500" color="black" className={styles.error}>
+                    Что-то пошло не так
+                  </Typography>
+                  <Button onClick={handleRetry}>ПОПРОБОВАТЬ ЕЩЕ РАЗ</Button>
+                </Flex>
+              )}
+            </Flex>
+          )}
+          {!isState && (
+            <>
+              <CommandPhoto total_points={game.total_points} />
+              <CommandContent {...game} />
+            </>
+          )}
         </div>
       </Flex>
       <Button onClick={() => navigate(NavigationRoutes.ROOT)}>Вернуться обратно</Button>
